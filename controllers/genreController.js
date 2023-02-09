bf75a5d3ce0da2a420090a374e3bbb753196c141
DrlngLnr <madowl052@gmail.com>
2023-02-10 02:41:51 +0300

    add basic CRUD for products, genres, authors

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,10 +3,13 @@ const ApiError = require('../error/ApiError');
 
 class GenreController {
   async create(req, res) {
-
+    const {name} = req.body;
+    const genre = await Genre.create({name});
+    return res.json(genre);
   }
   async getAll(req, res) {
-
+    const genres = await Genre.findAll();
+    return res.json(genres);
   }
   async delete(req, res) {
 
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,22 @@
-class GenreController {
-  async create(req, res) {
+const uuid = require('uuid');
+const path = require('path');
+const {Product} = require('../models/models');
+const ApiError = require('../error/ApiError');
 
+class ProductController {
+  async create(req, res, next) {
+    try {
+      const {name, description, price, slug, authorId, genreId} = req.body;
+      const {img} = req.files;
+      let filename = uuid.v4() + '.jpg';
+      img.mv(path.resolve(__dirname, '..', 'static', filename));
+  
+      const product = await Product.create({name, description, img: filename, price, slug, authorId, genreId});
+    
+      return res.json(product);
+    } catch(err) {
+      next(ApiError.badRequest(err.message));
+    }
   }
   async getAll(req, res) {
 
@@ -13,4 +29,4 @@ class GenreController {
   }
 }
 
-module.exports = new GenreController();
+module.exports = new ProductController();
