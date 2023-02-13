4d9d38aa28425171ec7e10f58dd3f5ac39be831c
DrlngLnr <madowl052@gmail.com>
2023-02-13 17:59:22 +0300

    add create, getAll, delete methods in favorite controller

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,12 +1,26 @@
+const {Favorite} = require('../models/models');
+
 class FavoriteController {
   async create(req, res) {
+    const {userId} = req.user;
+    const {productId} = req.body;
 
+    const favorite = await Favorite.create({userId, productId});
+
+    return res.json(favorite);
   }
   async getAll(req, res) {
+    const {userId} = req.user;
 
+    const favorites = await Favorite.findAll({where: {userId}});
+
+    return res.json(favorites);
   }
   async delete(req, res) {
+    const {id} = req.params;
+    const favorite = await Favorite.destroy({where: {id}});
 
+    return res.json(favorite);
   }
 }
 
